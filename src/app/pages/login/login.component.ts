98fad9191b4c9d566cce3e6e4b7c5e6bdbb65f73
doron <doron.miller@gmail.com>
2019-11-10 21:37:01 +0200

    Enabling automatic login only on production while constructing special util file in assets/p-env

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
 import {first} from 'rxjs/operators';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +11,30 @@ import {first} from 'rxjs/operators';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username: string = 'admin';
-  password: string = 'adminGong';
+  username: string;
+  password: string;
   public error: string;
 
   constructor(private authService: AuthService,
+              private http: HttpClient,
               private router: Router) {
 
   }
 
   ngOnInit() {
+    if (!environment.production) {
+      this.http.get('assets/p-env/p_config.json')
+        .subscribe((pConfig: { username: string, password: string }) => {
+            console.log('aaaa', pConfig);
+            const {username, password} = pConfig;
+            this.username = username;
+            this.password = password;
+          },
+          (error) => {
+            console.error('ERROR : p_config is only for DEV mode and is privately and locally constructed', error);
+          });
+    }
+
     if (this.authService.loggedIn) {
       this.router.navigate(['mainPage']);
     }
