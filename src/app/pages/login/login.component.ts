9aeb0e4d0e8fced5351d3df0ecdcc4f55f14d6f8
doron <doron.miller@gmail.com>
2019-11-14 15:25:19 +0200

    Setting the login with submitting the form - understand that no need to save login data - the browser can do it

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,8 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
 import {first} from 'rxjs/operators';
-import {environment} from '../../../environments/environment';
-import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -16,24 +14,11 @@ export class LoginComponent implements OnInit {
   public error: string;
 
   constructor(private authService: AuthService,
-              private http: HttpClient,
               private router: Router) {
 
   }
 
   ngOnInit() {
-    if (!environment.production) {
-      this.http.get('assets/p-env/p_config.json')
-        .subscribe((pConfig: { username: string, password: string }) => {
-            const {username, password} = pConfig;
-            this.username = username;
-            this.password = password;
-          },
-          (error) => {
-            console.error('ERROR : p_config is only for DEV mode and is privately and locally constructed', error);
-          });
-    }
-
     if (this.authService.loggedIn) {
       this.router.navigate(['mainPage']);
     }
